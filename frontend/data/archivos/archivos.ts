0197e3244653495fd803a40504ab334174ebe8cf
Alejandro Torres Jofré <ale.torres.jofre@gmail.com>
2023-07-20 19:17:06 -0400

    Añade tablas para archivos

--- frontend/data/archivos/archivos.ts
+++ frontend/data/archivos/archivos.ts
@@ -1,4 +1,4 @@
-
+import axios, { AxiosResponse } from "axios";
 
 export interface IArchivos {
     _id?: string;
@@ -13,3 +13,42 @@ export interface IArchivos {
     updatedAt?: Date;
     __v?: number;
 }
+
+const viewAllFiles = async () => {
+    const res = await axios.get(`${process.env.API_URL}/files/`);
+    return res.data;
+};
+
+const viewOneFile = async (id: string) => {
+    const res = await axios.get(`${process.env.API_URL}/file/specific/${id}`);
+    return res.data;
+};
+
+const viewFavorite = async (id: string) => {
+    const res = await axios.get(`${process.env.API_URL}/file/${id}`);
+    return res.data;
+};
+
+const downloadFile = async (id: string) => {
+    const res = await axios.get(`${process.env.API_URL}/file/download/${id}`);
+    return res.data;
+};
+
+const subirArchivo = async () => {
+    const res = await axios.get(`${process.env.API_URL}/file/upload/`);
+    return res.data;
+};
+
+const deleteFile = async (id: string) => {
+    const res = await axios.get(`${process.env.API_URL}/file/delete/${id}`);
+    return res.data;
+};
+
+export {
+    viewAllFiles,
+    viewOneFile,
+    viewFavorite,
+    downloadFile,
+    subirArchivo,
+    deleteFile,
+};
