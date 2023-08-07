c6b4559265ff1d810127dce5d0488e4f0550427c
Patricio <patricio.poblete1701@alumnos.ubiobio.cl>
2023-08-07 14:39:45 -0400

    hola

---
+++ backend/src/models/calendario/calendarioModel.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+export interface ItemCalendario {
+    nombreAct: string;
+    fechaInicio?: Date;
+    fechaTermino?: Date;
+    estadoActividad: boolean;
+    descripcion: string;
+}
+
+const calendarioSchema = new Schema<ItemCalendario>(
+    {
+        nombreAct: { type: String, required: true, minlength: 1, maxlength: 50 },
+        fechaInicio: { type: Date },
+        fechaTermino: { type: Date },
+        estadoActividad: { type: Boolean, required: true },
+        descripcion: { type: String, maxlength: 250 },
+    },
+    { timestamps: true, collection: "calendario" }
+);
+
+export const ItemCalendario2 = model<ItemCalendario>(
+    "ItemCalendario",
+    calendarioSchema
+);
